dc1e513b558e790386ede41214f7317160a50ddc
Lingda Li <lildmh@gmail.com>
2019-05-13 11:21:56 -0400

    Update codegen test

--- test/OpenMP/declare_mapper_codegen.cpp
+++ test/OpenMP/declare_mapper_codegen.cpp
@@ -40,9 +40,8 @@ public:
 
 #pragma omp declare mapper(id: C s) map(s.a, s.b[0:2])
 
-// Synchronous version of mapper function.
-// CK0-LABEL: define {{.*}}i32 @.omp_mapper.class_C.id{{.*}}(i8*, i8*, i8*, i{{64|32}}, i64)
-// CK0-DAG: store i8* %0, i64* [[HANDLEADDR:%[^,]+]]
+// CK0-LABEL: define {{.*}}void @.omp_mapper.class_C.id{{.*}}(i8*, i8*, i8*, i{{64|32}}, i64)
+// CK0-DAG: store i8* %0, i8** [[HANDLEADDR:%[^,]+]]
 // CK0-DAG: store i8* %1, i8** [[BPTRADDR:%[^,]+]]
 // CK0-DAG: store i8* %2, i8** [[VPTRADDR:%[^,]+]]
 // CK0-DAG: store i[[sz:64|32]] %3, i{{64|32}}* [[SIZEADDR:%[^,]+]]
@@ -52,6 +51,8 @@ public:
 // CK0-DAG: [[HANDLE:%.+]] = load i8*, i8** [[HANDLEADDR]]
 // CK0-DAG: [[PTRBEGIN:%.+]] = bitcast i8** [[VPTRADDR]] to %class.C**
 // CK0-DAG: [[PTREND:%.+]] = getelementptr %class.C*, %class.C** [[PTRBEGIN]], i[[sz]] [[SIZE]]
+// CK0-DAG: [[BPTR:%.+]] = load i8*, i8** [[BPTRADDR]]
+// CK0-DAG: [[BEGIN:%.+]] = load i8*, i8** [[VPTRADDR]]
 // CK0: [[ISARRAY:%.+]] = icmp sge i[[sz]] [[SIZE]], 1
 // CK0: br i1 [[ISARRAY]], label %[[INITEVALDEL:[^,]+]], label %[[LHEAD:[^,]+]]
 
@@ -60,15 +61,15 @@ public:
 // CK0: [[ISNOTDEL:%.+]] = icmp eq i64 [[TYPEDEL]], 0
 // CK0: br i1 [[ISNOTDEL]], label %[[INIT:[^,]+]], label %[[LHEAD:[^,]+]]
 // CK0: [[INIT]]
-// CK0-64: [[ARRSIZE:%.+]] = mul i[[sz]] [[SIZE]], 16
-// CK0-32: [[ARRSIZE:%.+]] = mul i[[sz]] [[SIZE]], 8
+// CK0-64-DAG: [[ARRSIZE:%.+]] = mul i[[sz]] [[SIZE]], 16
+// CK0-32-DAG: [[ARRSIZE:%.+]] = mul i[[sz]] [[SIZE]], 8
 // CK0-DAG: [[ITYPE:%.+]] = and i64 [[TYPE]], -4
-// CK0: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTRADDR]], i8* [[VPTRADDR]], i[[sz]] [[ARRSIZE]], i64 [[ITYPE]])
+// CK0: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTR]], i8* [[BEGIN]], i[[sz]] [[ARRSIZE]], i64 [[ITYPE]])
 // CK0: br label %[[LHEAD:[^,]+]]
 
 // CK0: [[LHEAD]]
 // CK0: [[ISEMPTY:%.+]] = icmp eq %class.C** [[PTRBEGIN]], [[PTREND]]
-// CK0: br i1 [[ISEMPTY]], label %[[DONE]], label %[[LBODY:[^,]+]]
+// CK0: br i1 [[ISEMPTY]], label %[[DONE:[^,]+]], label %[[LBODY:[^,]+]]
 // CK0: [[LBODY]]
 // CK0: [[PTR:%.+]] = phi %class.C** [ [[PTRBEGIN]], %[[LHEAD]] ], [ [[PTRNEXT:%.+]], %[[LCORRECT:[^,]+]] ]
 // CK0: [[OBJ:%.+]] = load %class.C*, %class.C** [[PTR]]
@@ -84,6 +85,7 @@ public:
 // CK0-DAG: [[BENDI:%.+]] = ptrtoint i8* [[BENDV]] to i64
 // CK0-DAG: [[CSIZE:%.+]] = sub i64 [[BENDI]], [[ABEGINI]]
 // CK0-DAG: [[CUSIZE:%.+]] = sdiv exact i64 [[CSIZE]], ptrtoint (i8* getelementptr (i8, i8* null, i32 1) to i64)
+// CK0-32-DAG: [[CUSIZE32:%.+]] = trunc i64 [[CUSIZE]] to i32
 // CK0-DAG: [[BPTRADDR0BC:%.+]] = bitcast %class.C* [[OBJ]] to i8*
 // CK0-DAG: [[PTRADDR0BC:%.+]] = bitcast i32* [[ABEGIN]] to i8*
 // CK0-DAG: [[TYPETF:%.+]] = and i64 [[TYPE]], 3
@@ -103,50 +105,69 @@ public:
 // CK0-DAG: br label %[[TYEND]]
 // CK0-DAG: [[TYEND]]
 // CK0-DAG: [[PHITYPE0:%.+]] = phi i64 [ 32, %[[ALLOC]] ], [ 32, %[[TO]] ], [ 32, %[[FROM]] ], [ 32, %[[TOELSE]] ]
-// CK0: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTRADDR0BC]], i8* [[PTRADDR0BC]], i[[sz]] [[CUSIZE]], i64 [[PHITYPE0]])
+// CK0-64: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTRADDR0BC]], i8* [[PTRADDR0BC]], i[[sz]] [[CUSIZE]], i64 [[PHITYPE0]])
+// CK0-32: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTRADDR0BC]], i8* [[PTRADDR0BC]], i[[sz]] [[CUSIZE32]], i64 [[PHITYPE0]])
+// CK0-DAG: [[BPTRADDR1BC:%.+]] = bitcast %class.C* [[OBJ]] to i8*
+// CK0-DAG: [[PTRADDR1BC:%.+]] = bitcast i32* [[ABEGIN]] to i8*
+// CK0-DAG: [[TYPETF:%.+]] = and i64 [[TYPE]], 3
+// CK0-DAG: [[ISALLOC:%.+]] = icmp eq i64 [[TYPETF]], 0
+// CK0-DAG: br i1 [[ISALLOC]], label %[[ALLOC:[^,]+]], label %[[ALLOCELSE:[^,]+]]
+// CK0-DAG: [[ALLOC]]
+// CK0-DAG: br label %[[TYEND:[^,]+]]
+// CK0-DAG: [[ALLOCELSE]]
+// CK0-DAG: [[ISTO:%.+]] = icmp eq i64 [[TYPETF]], 1
+// CK0-DAG: br i1 [[ISTO]], label %[[TO:[^,]+]], label %[[TOELSE:[^,]+]]
+// CK0-DAG: [[TO]]
+// CK0-DAG: br label %[[TYEND]]
+// CK0-DAG: [[TOELSE]]
+// CK0-DAG: [[ISFROM:%.+]] = icmp eq i64 [[TYPETF]], 2
+// CK0-DAG: br i1 [[ISFROM]], label %[[FROM:[^,]+]], label %[[TYEND]]
+// CK0-DAG: [[FROM]]
+// CK0-DAG: br label %[[TYEND]]
+// CK0-DAG: [[TYEND]]
 // 0x1,000,000,000; 0x1,000,000,001; 0x1,000,000,002; 0x1,000,000,003
 // CK0-DAG: [[TYPE1:%.+]] = phi i64 [ 281474976710656, %[[ALLOC]] ], [ 281474976710657, %[[TO]] ], [ 281474976710658, %[[FROM]] ], [ 281474976710659, %[[TOELSE]] ]
+// CK0: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTRADDR1BC]], i8* [[PTRADDR1BC]], i[[sz]] 4, i64 [[TYPE1]])
+// CK0-DAG: [[BPTRADDR2BC:%.+]] = bitcast double** [[BBEGIN]] to i8*
+// CK0-DAG: [[PTRADDR2BC:%.+]] = bitcast double* [[BARRBEGINGEP]] to i8*
+// CK0-DAG: [[TYPETF:%.+]] = and i64 [[TYPE]], 3
+// CK0-DAG: [[ISALLOC:%.+]] = icmp eq i64 [[TYPETF]], 0
+// CK0-DAG: br i1 [[ISALLOC]], label %[[ALLOC:[^,]+]], label %[[ALLOCELSE:[^,]+]]
+// CK0-DAG: [[ALLOC]]
+// CK0-DAG: br label %[[TYEND:[^,]+]]
+// CK0-DAG: [[ALLOCELSE]]
+// CK0-DAG: [[ISTO:%.+]] = icmp eq i64 [[TYPETF]], 1
+// CK0-DAG: br i1 [[ISTO]], label %[[TO:[^,]+]], label %[[TOELSE:[^,]+]]
+// CK0-DAG: [[TO]]
+// CK0-DAG: br label %[[TYEND]]
+// CK0-DAG: [[TOELSE]]
+// CK0-DAG: [[ISFROM:%.+]] = icmp eq i64 [[TYPETF]], 2
+// CK0-DAG: br i1 [[ISFROM]], label %[[FROM:[^,]+]], label %[[TYEND]]
+// CK0-DAG: [[FROM]]
+// CK0-DAG: br label %[[TYEND]]
+// CK0-DAG: [[TYEND]]
 // 0x1,000,000,010; 0x1,000,000,011; 0x1,000,000,012; 0x1,000,000,013
 // CK0-DAG: [[TYPE2:%.+]] = phi i64 [ 281474976710672, %[[ALLOC]] ], [ 281474976710673, %[[TO]] ], [ 281474976710674, %[[FROM]] ], [ 281474976710675, %[[TOELSE]] ]
-// CK0-DAG: store i64 [[TYPE2]], i64* [[TYPEADDR2]]
-// CK0-DAG: [[ARGBPTR:%.+]] = getelementptr inbounds [3 x i8*], [3 x i8*]* [[LBPTRADDR]], i32 0, i32 0
-// CK0-DAG: [[ARGPTR:%.+]] = getelementptr inbounds [3 x i8*], [3 x i8*]* [[PTRADDR]], i32 0, i32 0
-// CK0-DAG: [[ARGSIZE:%.+]] = getelementptr inbounds [3 x i[[sz]]], [3 x i[[sz]]]* [[SIZEADDR]], i32 0, i32 0
-// CK0-DAG: [[ARGTYPE:%.+]] = getelementptr inbounds [3 x i64], [3 x i64]* [[TYPEADDR]], i32 0, i32 0
-// CK0: [[RES:%.+]] = call i32 @__tgt_target_data_mapper(i64 [[HANDLE]], i32 3, i8** [[ARGBPTR]], i8** [[ARGPTR]], i[[sz]]* [[ARGSIZE]], i64* [[ARGTYPE]], i8** null)
-// CK0: [[ISERR:%.+]] = icmp ne i32 [[RES]], 0
-// CK0: br i1 [[ISERR]], label %[[LERR:[^,]+]], label %[[LCORRECT]]
-// CK0: [[LERR]]
-// CK0: store i32 [[RES]], i32* %retval
-// CK0: br label %[[DONE]]
-// CK0: [[LCORRECT]]
+// CK0: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTRADDR2BC]], i8* [[PTRADDR2BC]], i[[sz]] 16, i64 [[TYPE2]])
 // CK0: [[PTRNEXT]] = getelementptr %class.C*, %class.C** [[PTR]], i32 1
 // CK0: [[ISDONE:%.+]] = icmp eq %class.C** [[PTRNEXT]], [[PTREND]]
 // CK0: br i1 [[ISDONE]], label %[[LEXIT:[^,]+]], label %[[LBODY]]
 
 // CK0: [[LEXIT]]
+// CK0: [[ISARRAY:%.+]] = icmp sge i[[sz]] [[SIZE]], 1
 // CK0: br i1 [[ISARRAY]], label %[[EVALDEL:[^,]+]], label %[[DONE]]
 // CK0: [[EVALDEL]]
 // CK0: [[TYPEDEL:%.+]] = and i64 [[TYPE]], 8
 // CK0: [[ISDEL:%.+]] = icmp ne i64 [[TYPEDEL]], 0
 // CK0: br i1 [[ISDEL]], label %[[DEL:[^,]+]], label %[[DONE]]
 // CK0: [[DEL]]
-// CK0-64: [[ARRSIZE:%.+]] = mul i[[sz]] [[SIZE]], 16
-// CK0-32: [[ARRSIZE:%.+]] = mul i[[sz]] [[SIZE]], 8
-// CK0-DAG: store i[[sz]] [[ARRSIZE]], i[[sz]]* [[DSIZEADDR:%[^,]+]]
-// CK0-DAG: [[DSIZEADDR]] = getelementptr inbounds [1 x i[[sz]]], [1 x i[[sz]]]* [[DSIZE:%.+]], i32 0, i32 0
+// CK0-64-DAG: [[ARRSIZE:%.+]] = mul i[[sz]] [[SIZE]], 16
+// CK0-32-DAG: [[ARRSIZE:%.+]] = mul i[[sz]] [[SIZE]], 8
 // CK0-DAG: [[DTYPE:%.+]] = and i64 [[TYPE]], -4
-// CK0-DAG: store i64 [[DTYPE]], i64* [[DTYPEADDR:%[^,]+]]
-// CK0-DAG: [[DTYPEADDR]] = getelementptr inbounds [1 x i64], [1 x i64]* [[DTYPE:%.+]], i32 0, i32 0
-// CK0: [[DRES:%.+]] = call i32 @__tgt_target_data_mapper(i64 [[HANDLE]], i32 1, i8** [[BPTRADDR]], i8** [[VPTRADDR]], i[[sz]]* [[DSIZEADDR]], i64* [[DTYPEADDR]], i8** null)
-// CK0: [[ISDELERR:%.+]] = icmp ne i32 [[DRES]], 0
-// CK0: br i1 [[ISDELERR]], label %[[DELERR:[^,]+]], label %[[DONE]]
-// CK0: [[DELERR]]
-// CK0: store i32 [[DRES]], i32* %retval
+// CK0: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTR]], i8* [[BEGIN]], i[[sz]] [[ARRSIZE]], i64 [[DTYPE]])
 // CK0: br label %[[DONE]]
 // CK0: [[DONE]]
-// CK0: [[RET:%.+]] = load i32, i32* %retval
-// CK0: ret i32 [[RET]]
+// CK0: ret void
 
 
 // CK0-LABEL: define {{.*}}void @{{.*}}foo{{.*}}
@@ -231,19 +252,19 @@ public:
 
 #pragma omp declare mapper(id: C<int> s) map(s.a)
 
-// Synchronous version of mapper function.
-// CK1-LABEL: define {{.*}}i32 @".omp_mapper.C<int>.id{{.*}}(i64, i8*, i8*, i{{64|32}}, i64)
-// CK1-DAG: store i64 %0, i64* [[HANDLEADDR:%[^,]+]]
-// CK1-DAG: store i[[sz:64|32]] %3, i{{64|32}}* [[SIZEADDR:%[^,]+]]
-// CK1-DAG: store i64 %4, i64* [[TYPEADDR:%[^,]+]]
+// CK1-LABEL: define {{.*}}void @".omp_mapper.C<int>.id{{.*}}"(i8*, i8*, i8*, i{{64|32}}, i64)
+// CK1-DAG: store i8* %0, i8** [[HANDLEADDR:%[^,]+]]
 // CK1-DAG: store i8* %1, i8** [[BPTRADDR:%[^,]+]]
 // CK1-DAG: store i8* %2, i8** [[VPTRADDR:%[^,]+]]
-// CK1-DAG: store i32 0, i32* %retval
+// CK1-DAG: store i[[sz:64|32]] %3, i{{64|32}}* [[SIZEADDR:%[^,]+]]
+// CK1-DAG: store i64 %4, i64* [[TYPEADDR:%[^,]+]]
 // CK1-DAG: [[SIZE:%.+]] = load i[[sz]], i[[sz]]* [[SIZEADDR]]
 // CK1-DAG: [[TYPE:%.+]] = load i64, i64* [[TYPEADDR]]
-// CK1-DAG: [[HANDLE:%.+]] = load i64, i64* [[HANDLEADDR]]
+// CK1-DAG: [[HANDLE:%.+]] = load i8*, i8** [[HANDLEADDR]]
 // CK1-DAG: [[PTRBEGIN:%.+]] = bitcast i8** [[VPTRADDR]] to %class.C**
 // CK1-DAG: [[PTREND:%.+]] = getelementptr %class.C*, %class.C** [[PTRBEGIN]], i[[sz]] [[SIZE]]
+// CK1-DAG: [[BPTR:%.+]] = load i8*, i8** [[BPTRADDR]]
+// CK1-DAG: [[BEGIN:%.+]] = load i8*, i8** [[VPTRADDR]]
 // CK1: [[ISARRAY:%.+]] = icmp sge i[[sz]] [[SIZE]], 1
 // CK1: br i1 [[ISARRAY]], label %[[INITEVALDEL:[^,]+]], label %[[LHEAD:[^,]+]]
 
@@ -252,22 +273,14 @@ public:
 // CK1: [[ISNOTDEL:%.+]] = icmp eq i64 [[TYPEDEL]], 0
 // CK1: br i1 [[ISNOTDEL]], label %[[INIT:[^,]+]], label %[[LHEAD:[^,]+]]
 // CK1: [[INIT]]
-// CK1: [[ARRSIZE:%.+]] = mul i[[sz]] [[SIZE]], 4
-// CK1-DAG: store i[[sz]] [[ARRSIZE]], i[[sz]]* [[ISIZEADDR:[^,]+]]
-// CK1-DAG: [[ISIZEADDR]] = getelementptr inbounds [1 x i[[sz]]], [1 x i[[sz]]]* [[ISIZE:%.+]], i32 0, i32 0
+// CK1-DAG: [[ARRSIZE:%.+]] = mul i[[sz]] [[SIZE]], 4
 // CK1-DAG: [[ITYPE:%.+]] = and i64 [[TYPE]], -4
-// CK1-DAG: store i64 [[ITYPE]], i64* [[ITYPEADDR:[^,]+]]
-// CK1-DAG: [[ITYPEADDR]] = getelementptr inbounds [1 x i64], [1 x i64]* [[ITYPE:%.+]], i32 0, i32 0
-// CK1: [[IRES:%.+]] = call i32 @__tgt_target_data_mapper(i64 [[HANDLE]], i32 1, i8** [[BPTRADDR]], i8** [[VPTRADDR]], i[[sz]]* [[ISIZEADDR]], i64* [[ITYPEADDR]], i8** null)
-// CK1: [[ISINITERR:%.+]] = icmp ne i32 [[IRES]], 0
-// CK1: br i1 [[ISINITERR]], label %[[INITERR:[^,]+]], label %[[LHEAD:[^,]+]]
-// CK1: [[INITERR]]
-// CK1: store i32 [[IRES]], i32* %retval
-// CK1: br label %[[DONE:[^,]+]]
+// CK1: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTR]], i8* [[BEGIN]], i[[sz]] [[ARRSIZE]], i64 [[ITYPE]])
+// CK1: br label %[[LHEAD:[^,]+]]
 
 // CK1: [[LHEAD]]
 // CK1: [[ISEMPTY:%.+]] = icmp eq %class.C** [[PTRBEGIN]], [[PTREND]]
-// CK1: br i1 [[ISEMPTY]], label %[[DONE]], label %[[LBODY:[^,]+]]
+// CK1: br i1 [[ISEMPTY]], label %[[DONE:[^,]+]], label %[[LBODY:[^,]+]]
 // CK1: [[LBODY]]
 // CK1: [[PTR:%.+]] = phi %class.C** [ [[PTRBEGIN]], %[[LHEAD]] ], [ [[PTRNEXT:%.+]], %[[LCORRECT:[^,]+]] ]
 // CK1: [[OBJ:%.+]] = load %class.C*, %class.C** [[PTR]]
@@ -280,15 +293,8 @@ public:
 // CK1-DAG: [[CSIZE:%.+]] = sub i64 [[AENDI]], [[ABEGINI]]
 // CK1-DAG: [[CUSIZE:%.+]] = sdiv exact i64 [[CSIZE]], ptrtoint (i8* getelementptr (i8, i8* null, i32 1) to i64)
 // CK1-32-DAG: [[CUSIZE32:%.+]] = trunc i64 [[CUSIZE]] to i32
-// CK1-64-DAG: store i[[sz]] [[CUSIZE]], i[[sz]]* [[SIZEADDR0:%[^,]+]]
-// CK1-32-DAG: store i[[sz]] [[CUSIZE32]], i[[sz]]* [[SIZEADDR0:%[^,]+]]
-// CK1-DAG: [[SIZEADDR0]] = getelementptr inbounds [2 x i[[sz]]], [2 x i[[sz]]]* [[SIZEADDR:%[^,]+]], i32 0, i32 0
-// CK1-DAG: store %class.C* [[OBJ]], %class.C** [[BPTRADDR0BC:%[^,]+]]
-// CK1-DAG: [[BPTRADDR0BC]] = bitcast i8** [[BPTRADDR0:%.+]] to %class.C**
-// CK1-DAG: [[BPTRADDR0]] = getelementptr inbounds [2 x i8*], [2 x i8*]* [[LBPTRADDR:%[^,]+]], i32 0, i32 0
-// CK1-DAG: store i32* [[ABEGIN]], i32** [[PTRADDR0BC:%[^,]+]]
-// CK1-DAG: [[PTRADDR0BC]] = bitcast i8** [[PTRADDR0:%.+]] to i32**
-// CK1-DAG: [[PTRADDR0]] = getelementptr inbounds [2 x i8*], [2 x i8*]* [[PTRADDR:%[^,]+]], i32 0, i32 0
+// CK1-DAG: [[BPTRADDR0BC:%.+]] = bitcast %class.C* [[OBJ]] to i8*
+// CK1-DAG: [[PTRADDR0BC:%.+]] = bitcast i32* [[ABEGIN]] to i8*
 // CK1-DAG: [[TYPETF:%.+]] = and i64 [[TYPE]], 3
 // CK1-DAG: [[ISALLOC:%.+]] = icmp eq i64 [[TYPETF]], 0
 // CK1-DAG: br i1 [[ISALLOC]], label %[[ALLOC:[^,]+]], label %[[ALLOCELSE:[^,]+]]
@@ -306,17 +312,10 @@ public:
 // CK1-DAG: br label %[[TYEND]]
 // CK1-DAG: [[TYEND]]
 // CK1-DAG: [[PHITYPE0:%.+]] = phi i64 [ 32, %[[ALLOC]] ], [ 32, %[[TO]] ], [ 32, %[[FROM]] ], [ 32, %[[TOELSE]] ]
-// CK1-DAG: store i64 [[PHITYPE0]], i64* [[TYPEADDR0:%[^,]+]]
-// CK1-DAG: [[TYPEADDR0]] = getelementptr inbounds [2 x i64], [2 x i64]* [[TYPEADDR:%[^,]+]], i32 0, i32 0
-// CK1-DAG: [[BPTRADDR1:%.+]] = getelementptr inbounds [2 x i8*], [2 x i8*]* [[LBPTRADDR]], i32 0, i32 1
-// CK1-DAG: [[BPTRADDR1BC:%.+]] = bitcast i8** [[BPTRADDR1]] to %class.C**
-// CK1-DAG: store %class.C* [[OBJ]], %class.C** [[BPTRADDR1BC]]
-// CK1-DAG: [[PTRADDR1:%.+]] = getelementptr inbounds [2 x i8*], [2 x i8*]* [[PTRADDR]], i32 0, i32 1
-// CK1-DAG: [[PTRADDR1BC:%.+]] = bitcast i8** [[PTRADDR1]] to i32**
-// CK1-DAG: store i32* [[ABEGIN]], i32** [[PTRADDR1BC]]
-// CK1-DAG: [[SIZEADDR1:%.+]] = getelementptr inbounds [2 x i[[sz]]], [2 x i[[sz]]]* [[SIZEADDR]], i32 0, i32 1
-// CK1-DAG: store i[[sz]] 4, i[[sz]]* [[SIZEADDR1]]
-// CK1-DAG: [[TYPEADDR1:%.+]] = getelementptr inbounds [2 x i64], [2 x i64]* [[TYPEADDR]], i32 0, i32 1
+// CK1-64: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTRADDR0BC]], i8* [[PTRADDR0BC]], i[[sz]] [[CUSIZE]], i64 [[PHITYPE0]])
+// CK1-32: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTRADDR0BC]], i8* [[PTRADDR0BC]], i[[sz]] [[CUSIZE32]], i64 [[PHITYPE0]])
+// CK1-DAG: [[BPTRADDR1BC:%.+]] = bitcast %class.C* [[OBJ]] to i8*
+// CK1-DAG: [[PTRADDR1BC:%.+]] = bitcast i32* [[ABEGIN]] to i8*
 // CK1-DAG: [[TYPETF:%.+]] = and i64 [[TYPE]], 3
 // CK1-DAG: [[ISALLOC:%.+]] = icmp eq i64 [[TYPETF]], 0
 // CK1-DAG: br i1 [[ISALLOC]], label %[[ALLOC:[^,]+]], label %[[ALLOCELSE:[^,]+]]
@@ -335,44 +334,25 @@ public:
 // CK1-DAG: [[TYEND]]
 // 0x1,000,000,000; 0x1,000,000,001; 0x1,000,000,002; 0x1,000,000,003
 // CK1-DAG: [[TYPE1:%.+]] = phi i64 [ 281474976710656, %[[ALLOC]] ], [ 281474976710657, %[[TO]] ], [ 281474976710658, %[[FROM]] ], [ 281474976710659, %[[TOELSE]] ]
-// CK1-DAG: store i64 [[TYPE1]], i64* [[TYPEADDR1]]
-// CK1-DAG: [[ARGBPTR:%.+]] = getelementptr inbounds [2 x i8*], [2 x i8*]* [[LBPTRADDR]], i32 0, i32 0
-// CK1-DAG: [[ARGPTR:%.+]] = getelementptr inbounds [2 x i8*], [2 x i8*]* [[PTRADDR]], i32 0, i32 0
-// CK1-DAG: [[ARGSIZE:%.+]] = getelementptr inbounds [2 x i[[sz]]], [2 x i[[sz]]]* [[SIZEADDR]], i32 0, i32 0
-// CK1-DAG: [[ARGTYPE:%.+]] = getelementptr inbounds [2 x i64], [2 x i64]* [[TYPEADDR]], i32 0, i32 0
-// CK1: [[RES:%.+]] = call i32 @__tgt_target_data_mapper(i64 [[HANDLE]], i32 2, i8** [[ARGBPTR]], i8** [[ARGPTR]], i[[sz]]* [[ARGSIZE]], i64* [[ARGTYPE]], i8** null)
-// CK1: [[ISERR:%.+]] = icmp ne i32 [[RES]], 0
-// CK1: br i1 [[ISERR]], label %[[LERR:[^,]+]], label %[[LCORRECT]]
-// CK1: [[LERR]]
-// CK1: store i32 [[RES]], i32* %retval
-// CK1: br label %[[DONE]]
-// CK1: [[LCORRECT]]
+// CK1: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTRADDR1BC]], i8* [[PTRADDR1BC]], i[[sz]] 4, i64 [[TYPE1]])
 // CK1: [[PTRNEXT]] = getelementptr %class.C*, %class.C** [[PTR]], i32 1
 // CK1: [[ISDONE:%.+]] = icmp eq %class.C** [[PTRNEXT]], [[PTREND]]
 // CK1: br i1 [[ISDONE]], label %[[LEXIT:[^,]+]], label %[[LBODY]]
 
 // CK1: [[LEXIT]]
+// CK1: [[ISARRAY:%.+]] = icmp sge i[[sz]] [[SIZE]], 1
 // CK1: br i1 [[ISARRAY]], label %[[EVALDEL:[^,]+]], label %[[DONE]]
 // CK1: [[EVALDEL]]
 // CK1: [[TYPEDEL:%.+]] = and i64 [[TYPE]], 8
 // CK1: [[ISDEL:%.+]] = icmp ne i64 [[TYPEDEL]], 0
 // CK1: br i1 [[ISDEL]], label %[[DEL:[^,]+]], label %[[DONE]]
 // CK1: [[DEL]]
-// CK1: [[ARRSIZE:%.+]] = mul i[[sz]] [[SIZE]], 4
-// CK1-DAG: store i[[sz]] [[ARRSIZE]], i[[sz]]* [[DSIZEADDR:%[^,]+]]
-// CK1-DAG: [[DSIZEADDR]] = getelementptr inbounds [1 x i[[sz]]], [1 x i[[sz]]]* [[DSIZE:%.+]], i32 0, i32 0
+// CK1-DAG: [[ARRSIZE:%.+]] = mul i[[sz]] [[SIZE]], 4
 // CK1-DAG: [[DTYPE:%.+]] = and i64 [[TYPE]], -4
-// CK1-DAG: store i64 [[DTYPE]], i64* [[DTYPEADDR:%[^,]+]]
-// CK1-DAG: [[DTYPEADDR]] = getelementptr inbounds [1 x i64], [1 x i64]* [[DTYPE:%.+]], i32 0, i32 0
-// CK1: [[DRES:%.+]] = call i32 @__tgt_target_data_mapper(i64 [[HANDLE]], i32 1, i8** [[BPTRADDR]], i8** [[VPTRADDR]], i[[sz]]* [[DSIZEADDR]], i64* [[DTYPEADDR]], i8** null)
-// CK1: [[ISDELERR:%.+]] = icmp ne i32 [[DRES]], 0
-// CK1: br i1 [[ISDELERR]], label %[[DELERR:[^,]+]], label %[[DONE]]
-// CK1: [[DELERR]]
-// CK1: store i32 [[DRES]], i32* %retval
+// CK1: call void @__kmpc_push_mapper_component(i8* [[HANDLE]], i8* [[BPTR]], i8* [[BEGIN]], i[[sz]] [[ARRSIZE]], i64 [[DTYPE]])
 // CK1: br label %[[DONE]]
 // CK1: [[DONE]]
-// CK1: [[RET:%.+]] = load i32, i32* %retval
-// CK1: ret i32 [[RET]]
+// CK1: ret void
 
 #endif
 
